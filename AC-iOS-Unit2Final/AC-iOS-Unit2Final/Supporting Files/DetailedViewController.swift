49f3339f8c7f02fa4aedfd2c08ba0a465c53f726
Richard Crichlow <richardcrichlow@ac.c4q.nyc>
2017-11-10 13:00:11 -0500

    finished label color change extra credit

--- AC-iOS-Unit2Final/AC-iOS-Unit2Final/Supporting Files/DetailedViewController.swift
+++ AC-iOS-Unit2Final/AC-iOS-Unit2Final/Supporting Files/DetailedViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class DetailedViewController: UIViewController {
     
+    //The Instance of the Segued Cell
+    var detailedCrayon: Crayon?
+    
     //Labels
     @IBOutlet weak var detailedCrayonNameLabel: UILabel!
     @IBOutlet weak var redLabel: UILabel!
@@ -17,19 +20,82 @@ class DetailedViewController: UIViewController {
     @IBOutlet weak var blueLabel: UILabel!
     @IBOutlet weak var alphaLabel: UILabel!
     
+    var allLabels: [UILabel] = []
+    
     //Actions
+    var sliderRedValue = Float() {
+        didSet {
+            self.redLabel.text = "Red: " + String(sliderRedValue)
+            self.colorView.backgroundColor = UIColor(displayP3Red: CGFloat(sliderRedValue), green: CGFloat(sliderGreenValue), blue: CGFloat(sliderBlueValue), alpha: CGFloat(stepperNumber))
+            if sliderRedValue < 0.1 {
+                allLabels = [detailedCrayonNameLabel, redLabel, greenLabel, blueLabel, alphaLabel]
+                for label in allLabels {
+                    label.backgroundColor = UIColor.darkGray
+                }
+            } else {
+                for label in allLabels {
+                    label.backgroundColor = UIColor.clear
+                }
+            }
+        }
+    }
     @IBAction func redSliderAction(_ sender: UISlider) {
+        self.sliderRedValue = sender.value
+    }
+    var sliderGreenValue = Float() {
+        didSet {
+            self.greenLabel.text = "Green: " + String(sliderGreenValue)
+            self.colorView.backgroundColor = UIColor(displayP3Red: CGFloat(sliderRedValue), green: CGFloat(sliderGreenValue), blue: CGFloat(sliderBlueValue), alpha: CGFloat(stepperNumber))
+            if sliderGreenValue < 0.1 {
+                allLabels = [detailedCrayonNameLabel, redLabel, greenLabel, blueLabel, alphaLabel]
+                for label in allLabels {
+                    label.backgroundColor = UIColor.darkGray
+                }
+            } else {
+                for label in allLabels {
+                    label.backgroundColor = UIColor.clear
+                }
+            }
+        }
     }
     @IBAction func greenSliderAction(_ sender: UISlider) {
+        self.sliderGreenValue = sender.value
+    }
+    var sliderBlueValue = Float() {
+        didSet {
+            self.blueLabel.text = "Blue: " + String(sliderBlueValue)
+            self.colorView.backgroundColor = UIColor(displayP3Red: CGFloat(sliderRedValue), green: CGFloat(sliderGreenValue), blue: CGFloat(sliderBlueValue), alpha: CGFloat(stepperNumber))
+            if sliderBlueValue < 0.1 {
+                allLabels = [detailedCrayonNameLabel, redLabel, greenLabel, blueLabel, alphaLabel]
+                for label in allLabels {
+                    label.backgroundColor = UIColor.darkGray
+                }
+            } else {
+                for label in allLabels {
+                    label.backgroundColor = UIColor.clear
+                }
+            }
+        }
     }
     @IBAction func blueSliderAction(_ sender: UISlider) {
+        self.sliderBlueValue = sender.value
+    }
+    var stepperNumber: Double = 1.0 {
+        didSet {
+            alphaLabel.text = "Alpha: \(stepperNumber)"
+            alphaStepperOutlet.value = stepperNumber
+            self.colorView.backgroundColor = UIColor(displayP3Red: CGFloat(sliderRedValue), green: CGFloat(sliderGreenValue), blue: CGFloat(sliderBlueValue), alpha: CGFloat(stepperNumber))
+        }
     }
     @IBAction func alphaStepperAction(_ sender: UIStepper) {
+        stepperNumber = sender.value
     }
     @IBAction func resetButton(_ sender: UIButton) {
+        reset()
     }
     
     //Outlets
+    @IBOutlet weak var colorView: UIView!
     @IBOutlet weak var redSliderOutlet: UISlider!
     @IBOutlet weak var greenSliderOutlet: UISlider!
     @IBOutlet weak var blueSliderOutlet: UISlider!
@@ -38,10 +104,34 @@ class DetailedViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        guard let detailedCrayon = detailedCrayon else {
+            return
+        }//This is to make sure there is a Crayon to show details for
+        
+        detailedCrayonNameLabel.text = detailedCrayon.name
+        reset()
         
     }
-
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        reset()
+    }
+    
+    func reset() {
+        stepperNumber = 1.0
+        sliderRedValue = (Float((detailedCrayon?.red)! / 255))
+        sliderGreenValue = (Float((detailedCrayon?.green)! / 255))
+        sliderBlueValue = (Float((detailedCrayon?.blue)! / 255))
+        alphaStepperOutlet.value = 1.0
+        redSliderOutlet.value = sliderRedValue
+        greenSliderOutlet.value = sliderGreenValue
+        blueSliderOutlet.value = sliderBlueValue
+        self.colorView.backgroundColor = UIColor(displayP3Red: CGFloat(sliderRedValue), green: CGFloat(sliderGreenValue), blue: CGFloat(sliderBlueValue), alpha: 1)
+        alphaLabel.text = "Alpha: \(stepperNumber)"
+        self.greenLabel.text = "Green: " + String(sliderGreenValue)
+        self.blueLabel.text = "Blue: " + String(sliderBlueValue)
+        self.redLabel.text = "Red: " + String(sliderRedValue)
+    }
 
 }
--- AC-iOS-Unit2Final/AC-iOS-Unit2Final/Supporting Files/MainViewController.swift
+++ AC-iOS-Unit2Final/AC-iOS-Unit2Final/Supporting Files/MainViewController.swift
@@ -8,14 +8,38 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
-
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var theCrayons = [Crayon]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        theCrayons = Crayon.allTheCrayons
     }
-
     
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return theCrayons.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let crayon = theCrayons[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "colorCell", for: indexPath)
+        
+        cell.backgroundColor = UIColor(displayP3Red: CGFloat(crayon.red / 255), green: CGFloat(crayon.green / 255), blue: CGFloat(crayon.blue / 255), alpha: 1)
+        cell.textLabel?.text = crayon.name
+        cell.detailTextLabel?.text = "Hex Value: \(crayon.hex)"
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let destination = segue.destination as? DetailedViewController {
+            let selectedRow = self.tableView.indexPathForSelectedRow!.row
+            let selectedCrayon = self.theCrayons[selectedRow]
+            destination.detailedCrayon = selectedCrayon
+        }
+    }
 }
