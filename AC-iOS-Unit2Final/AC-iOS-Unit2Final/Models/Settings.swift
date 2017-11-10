068a54a474f6f2a0fc73ef7d4c4d2ac486c7d1a3
Gloria <gloriaewashington@gmail.com>
2017-11-10 17:36:53 -0500

    Unit 2 Final Complete

---
+++ AC-iOS-Unit2Final/AC-iOS-Unit2Final/Models/Settings.swift
@@ -0,0 +1,17 @@
+//
+//  Settings.swift
+//  AC-iOS-Unit2Final
+//
+//  Created by C4Q on 11/10/17.
+//  Copyright © 2017 Karen Fuentes. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Settings {
+    static var red = CGFloat()
+    static var blue = CGFloat()
+    static var green = CGFloat()
+    static var alpha = CGFloat()
+}
