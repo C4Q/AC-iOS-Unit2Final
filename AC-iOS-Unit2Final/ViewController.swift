51fe877f74b281bdd37477825bf1963a549ce87d
Lisa <imnotlostrui@aim.com>
2017-11-10 16:09:03 -0500

    unit 2 final

--- AC-iOS-Unit2Final/ViewController.swift
+++ AC-iOS-Unit2Final/ViewController.swift
@@ -8,28 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    var crayonArr = [Crayon]()
+    @IBOutlet weak var tableView: UITableView!
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return crayonArr.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let rColor = (crayonArr[indexPath.row].red)
+        let gColor = (crayonArr[indexPath.row].green)
+        let bColor = (crayonArr[indexPath.row].blue)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "crayonCell", for: indexPath)
+        cell.textLabel?.text = crayonArr[indexPath.row].name
+        cell.detailTextLabel?.text = crayonArr[indexPath.row].hex
+        cell.backgroundColor = UIColor(displayP3Red: CGFloat(rColor/255), green: CGFloat(gColor/255), blue:CGFloat(bColor/255), alpha: 1)
+        return cell 
     }
-    */
-
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        loadData()
+        
+        // Do any additional setup after loading the view.
+    }
+    func loadData() {
+      crayonArr = Crayon.allTheCrayons//loads all the crayon colors
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "detailSegue" {
+            print("segue works")
+            if let destination = segue.destination as? detailViewController {
+                destination.crayon = crayonArr[tableView.indexPathForSelectedRow!.row]
+                print("set segue destination")
+            }
+        }
+    }
 }
--- AC-iOS-Unit2Final/detailViewController.swift
+++ AC-iOS-Unit2Final/detailViewController.swift
@@ -9,27 +9,120 @@
 import UIKit
 
 class detailViewController: UIViewController {
-
+    var crayon: Crayon?
+    var currentRedVal: Double = 0.5 {
+        didSet {
+            self.view.backgroundColor = UIColor(displayP3Red: CGFloat(currentRedVal) , green: CGFloat(crayon!.green/255), blue: CGFloat(crayon!.blue/255), alpha: CGFloat(stepperCurrentVal))
+        }
+    }
+    var currentGreenVal: Double = 0.5 {
+        didSet {
+            self.view.backgroundColor = UIColor(displayP3Red: CGFloat(crayon!.red), green: CGFloat(currentGreenVal), blue: CGFloat(crayon!.blue), alpha: CGFloat(stepperCurrentVal))
+        }
+    }
+    var currentBlueVal: Double = 0.5 {
+        didSet {
+            self.view.backgroundColor = UIColor(displayP3Red: CGFloat(crayon!.red
+            ), green: CGFloat(crayon!.green), blue: CGFloat(currentBlueVal), alpha: CGFloat(stepperCurrentVal))
+        }
+    }
+    var stepperCurrentVal: Double = 1 {
+        didSet{
+            stepperLabel.text = "alpha: \(stepperCurrentVal)"
+            stepper.value = stepperCurrentVal
+            
+        print(currentRedVal)
+        }
+    }
+    //var currentGreenVal: Double = 0.5
+  //  var currentBlueVal: Double
+    @IBOutlet weak var sliderRVal: UISlider!
+    @IBOutlet weak var sliderGVal: UISlider!
+    @IBOutlet weak var sliderBVal: UISlider!
+    @IBOutlet weak var colorNameLabel: UILabel!
+    @IBOutlet weak var rLabel: UILabel!
+    @IBOutlet weak var bLabel: UILabel!
+    @IBOutlet weak var gLabel: UILabel!
+    @IBOutlet weak var stepperLabel: UILabel!
+    @IBOutlet weak var stepper: UIStepper!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        guard let crayon = crayon else {
+         return
+        }
+        colorNameLabel.text = crayon.name
+        self.view.backgroundColor = UIColor(displayP3Red:CGFloat(crayon.red/255) , green: CGFloat(crayon.green/255), blue: CGFloat(crayon.blue/255), alpha: CGFloat(stepperCurrentVal))
+        rLabel.text = "current red val: \(Int(crayon.red))"
+        gLabel.text = "current green val: \(Int(crayon.green))"
+        bLabel.text = "current blue val: \(Int(crayon.blue))"
+        sliderRVal.value = Float(crayon.red/255)// sets initial slider position
+        sliderGVal.value = Float(crayon.green/255)
+        sliderBVal.value = Float(crayon.blue/255)
+        stepperCurrentVal = stepper.value
+        stepperLabel.text = "alpha: \(stepperCurrentVal)"
+        
+        }
+    func lighterColors(){
+        if currentRedVal < 0.4 || currentGreenVal < 0.4 || stepperCurrentVal < 0.7 {
+        rLabel.textColor = UIColor.white
+        gLabel.textColor = UIColor.white
+        bLabel.textColor = UIColor.white
+        stepperLabel.textColor = UIColor.white
+        colorNameLabel.textColor = UIColor.white
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func rSliderValueChanged(_ sender: UISlider) {
+        
+        self.currentRedVal = Double(sender.value)
+        self.rLabel.text = "current red val: \(Int(sender.value*255))"// says slider value is 0-1
+        print(stepperCurrentVal)
+        lighterColors()
+        
     }
-    */
+    
+    @IBAction func gSliderValueChanged(_ sender: UISlider) {
+        self.currentGreenVal = Double(sender.value)
+        self.gLabel.text = "current green val: \(Int(sender.value*255))"
+        lighterColors()
+    }
+    
+    @IBAction func bSliderValueChanged(_ sender: UISlider) {
+        self.currentBlueVal = Double(sender.value)
+        self.bLabel.text = "current blue val: \(Int(sender.value*255))"
+        lighterColors()
+    }
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        stepperCurrentVal = sender.value
+        if stepperCurrentVal != 0 {
+        self.view.backgroundColor = UIColor(displayP3Red: CGFloat(currentRedVal), green: CGFloat(currentGreenVal), blue: CGFloat(currentBlueVal), alpha: CGFloat(stepperCurrentVal))
+        }else{
+            print("not working")
+        }
+        if stepperCurrentVal < 0.7  {
+            lighterColors()
+
+        }
+        
+    }
+  
+   
+    @IBAction func resetButton(_ sender: UIButton) {
+        self.view.backgroundColor = UIColor(displayP3Red: CGFloat(crayon!.red/255) , green: CGFloat(crayon!.green/255), blue: CGFloat(crayon!.blue/255), alpha: 1)
+        rLabel.text = "current red val: \(Int(crayon!.red))"
+        gLabel.text = "current green val: \(Int(crayon!.green))"
+        bLabel.text = "current blue val: \(Int(crayon!.blue))"
+        stepperCurrentVal = 1
+        stepper.value = 1
+        sliderRVal.value = Float(crayon!.red/255)
+        sliderGVal.value = Float(crayon!.green/255)
+        sliderBVal.value = Float(crayon!.blue/255)
+        rLabel.textColor = UIColor.black
+        gLabel.textColor = UIColor.black
+        bLabel.textColor = UIColor.black
+        stepperLabel.textColor = UIColor.black
+        colorNameLabel.textColor = UIColor.black
+        print("reset")
+    }
 
 }
